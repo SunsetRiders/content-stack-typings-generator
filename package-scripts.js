e9602df863b47143472e33a7a33ef15a919efa0c
Daniel Ferreira Monteiro Alves <dalves@daitangroup.com>
2018-07-11 10:52:07 -0300

    feat(build): Generate changelog.

--- package-scripts.js
+++ package-scripts.js
@@ -2,6 +2,14 @@ const utils = require('nps-utils');
 
 module.exports = {
   scripts: {
+    'hidden': {
+      'up-major': utils.series('changelog -M', 'yarn version --major --no-git-tag-version'),
+      'up-minor': utils.series('changelog -m', 'yarn version --minor --no-git-tag-version'),
+      'up-patch': utils.series('changelog -p', 'yarn version --patch --no-git-tag-version'),
+      'gitadd': utils.series('git add package.json', 'git add CHANGELOG.md'),
+      'gitcommit': 'git commit -m "Updated version and changelog."'
+    },
+
     'clear': {
       'default': utils.rimraf('./lib')
     },
@@ -9,6 +17,12 @@ module.exports = {
     'build': {
       'typescript': 'tsc -p .',
       'default': 'nps clear build.typescript'
+    },
+
+    'release': {
+      'major': utils.series('nps hidden.up-major', 'nps hidden.gitadd', 'nps hidden.gitcommit'),
+      'minor': utils.series('nps hidden.up-minor', 'nps hidden.gitadd', 'nps hidden.gitcommit'),
+      'patch': utils.series('nps hidden.up-patch', 'nps hidden.gitadd', 'nps hidden.gitcommit'),
     }
   }
 };
